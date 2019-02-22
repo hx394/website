8cfb9ca59f6d566878b27dbb55f5b4d26b7c9417
Vincent (Hongzhen) Xu <Vincent.Xu@nyu.edu>
2019-02-22 13:09:40 -0500

    ss

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(function(req,res,next){
 	});
 });
 
-http.get('*', function(req, res) {
+app.get('*', function(req, res) {
     res.redirect('https://' + req.headers.host + req.url);
 
     // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
